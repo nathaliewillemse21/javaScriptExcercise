31922b59f42c54898cfe44dc21a21cfaa6a9ae81
nathaliewillemse21 <njpwillemse69@gmail.com>
2023-11-10 14:06:43 +0200

    done

--- script/code.js
+++ script/code.js
@@ -1,4 +1,154 @@
-let numb1 
- let numb2 
-console.log(numb1 + numb2)
+// let numbers =[1, 3, 5]
+
+//  numbers.splice(1, 0, 2,)
+// numbers.splice(3, 0, 4)
+// console.log(numbers);
+//  numbers.splice(5,0, 6, 7, 'Borahae' )
+//  console.log(numbers)
+
+
+
+//   let data = [7, 3, 2, 4, 6, 'Abc', 1, 'abc', 6, -1]
+// console.log(data);
+//reverse
+// console.log(data.reverse());
+//sorting
+// console.log(data.sort());
+// console.log(data);
+// let numbers = data.filter(onlyNumbers)
+
+//let numbers = data.filter(a => a% 2 >=1)//for even numbers
+//let numbers = data.filter((a) => a % 2 == 0);
+
+//let numbers = data.filter((a) => typeof a == 'number');/*different way */
+//named function or function declaration
+
+// function onlyNumbers(a) {
+//           return typeof a == 'number'
+// }
+
+// console.log(numbers)
+
+// //create a array
+ //let data = [3, 5, 8,
+//     'Nathalie', 'Taryn']
+// console.log(data);
+// console.log(`Array size:${data.length}`);
+// console.log(`Element at position 3: ${data[3]}`);
+// data.push('Matthew')
+// console.log(data.at(-1));
+//to display the last element
+// console.log(data);
+
+// data.unshift('Hinata', 'Itachi')
+// //  data.unshift('Hinata', 'Itachi') //put the new elements in front
+// console.log(data)
+
+// data.shift()
+// console.log("After shift");
+// // to remove the first element
+// console.log(data);
+
+// data.pop() //to delete last element
+// console.log(data)
+// console.log("Before slicing")
+// console.log(data.slice(1, 3));
+
+//splice = add or remove
+// 0=> adding
+// 1=> Number(s)of elements to remove
+// console.log("===Adding using splice ");
+// data.splice(3, 0, 'Princess')
+// console.log(data)
+
+// let lastRemovedElement = data.pop()
+// console.log(lastRemovedElement);
+// console.log(lastRemovedElement);
+// let newArr = data.concat(lastRemovedElement)
+// console.log(newArr);
+
+
+
+// console.log(`Array size :$ {numbers.length}`);
+
+// console.table(numbers);
+
+//{numbers[3]} to specify the position of nathalie
+
+
+// Array:Collection of dat
+
+
+
+
+// // Create a Function
+
+// const btnAdd = document.querySelector('[data-submit]')
+//    const lblOutput = document.querySelector('#output')
+
+
+// function addition() {
+//     let numb1 = document.getElementById('numb1').value
+//     let numb2 = document.getElementById('numb2').value
+//     let sum = +numb1 + +numb2
+//     lblOutput.innerHTML = `<strong>${sum}
+// //    lblOutput.textContent = sum
+// }
+// btnClick.addEventListener('click',addition)
+// // automatic way of creating a variable
+// function testing() {
+//     let numb1 = 23
+//     console.log(numb1);
+
+// }
+// testing()
+// // console.log(numb1);
+// // console.log(window);
+// console.log(globalThis);
+
+
+
+
+// let sentence = "I am going to work"
+//  console.log(sentence);
+// console.log(sentence.at(-1));
+// console.log(sentence.length);
+// let size = sentence.length
+// console.log(size);
+// console.log(sentence[size -1]);
+// console.log(sentence.slice(0, -1)); // remover last character from sentence
+// console.log(sentence);
+// console.log(sentence.toUpperCase());
+// console.log(sentence.toLowerCase());
+// console.log(sentence.includes('work'));
+// console.log(sentence.split("")); // to check how may words there are in your array
+// let numbers = [9, 8, 3]
+// console.log(numbers);
+// console.log(typeof numbers);
+
+let person = {
+    name: 'Nathalie',
+    surname: 'Wilemse',
+    age: 30,
+    subjects: ['HTML', 'CSS3', 'JavaScript']
+}
+console.log(person)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
